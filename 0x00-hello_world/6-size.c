1f2e218517c5d33c0d55e70c3c2a1a599c046973
QulinJ <qulinj27@gmail.com>
2023-02-16 04:22:04 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("Size of a char: %1 bytes(s)\n", sizeof(char));
-	printf("Size of an int: %1 byte(s)\n", sizeof(char));
+	printf("Size of an int: %1 byte(s)\n", sizeof(int));
 	printf("Size of long int: %1 byte(s)\n", sizeof(long int));
 	ptintf("Size of long long int: %1 byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %1 byte(s)\n", sizeof(float));
