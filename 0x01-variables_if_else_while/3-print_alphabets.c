e95c094b6ab083919190687c0c6a2c8f958dda45
QulinJ <qulinj27@gmail.com>
2023-02-19 21:57:05 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * structure: 0 (Success)
+ */
+int main(void)
+{
+	int s = 97, e = 122;
+	while(s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+	s = 65;
+	e = 90;
+	while (s<= e)
+	{
+		putchar(s);
+		s++;
+	}
+	putchar('\n);
+	return(0);
+}
