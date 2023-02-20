12c8bfb5947f388752321c7b84ce1254e8637ea7
QulinJ <qulinj27@gmail.com>
2023-02-19 21:41:34 -0800

    PrintAlphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints alpha in lower and uppercase
- * Return: Always 0(Success)
+ * main - prints the alphabet.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; 1 < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
