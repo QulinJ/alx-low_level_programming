3f424f90d1144378fb864e3ad38ce1301d93aa61
QulinJ <qulinj27@gmail.com>
2023-02-22 23:48:52 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiples low integers.
+ * @a: The first integer to be multiplied is.
+ * @b: The second integer to be multiplied is.
+ *
+ * Return: The result of multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a = b);
+}
