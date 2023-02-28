7de9121f31f36ebdc8c05c5b3b7a6d57bb576654
QulinJ <qulinj27@gmail.com>
2023-02-28 09:09:45 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: The int type
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+
+	_putchar(s[i]);
+
+	_putchar('\n');
+}
